43dd0904c3cb5a687cce6c21625a98349074d4fd
Hongyou Li <33373020+HongyouLi@users.noreply.github.com>
2017-11-12 11:34:51 +0000

    Session_1 update
    add dictionary

---
+++ Assets/Scripts/school/Session_2.cs
@@ -0,0 +1,40 @@
+﻿//Session_2: Conditionals, Loops nad Classes
+//UCL RC3 12NOV 2017
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session_2 : MonoBehaviour {
+
+    public int myNumber = 2;
+    bool questionTime = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Conditionals
+        /*
+        if (myNumber == 2)
+        {
+            Debug.Log("Your number is equal to 2.");
+        }
+        else
+        {
+            Debug.Log("Your number is not equal to 2.");
+        }
+        */
+        if (questionTime) {
+            Debug.Log("Question time is false.");
+        }
+        else
+        {
+            Debug.Log("Question time is true.");
+        }
+        
+    }
+}
