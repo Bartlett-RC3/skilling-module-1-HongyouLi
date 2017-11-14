e86c40d5c93b3a02bd3ad95664ec4ae4e55c8136
Hongyou Li <33373020+HongyouLi@users.noreply.github.com>
2017-11-14 21:06:13 +0000

    Session_4

---
+++ Assets/Scripts/school/Session_4.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session_4 : MonoBehaviour {
+
+    //Variable
+    public GameObject columnPrefab;
+    IEnumerator createColumnCoroutine;
+
+	// Use this for initialization
+	void Start () {
+        createColumnCoroutine = DropColumns();
+        StartCoroutine(createColumnCoroutine);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {   ///getkey is hold  down up 
+            StopCoroutine(createColumnCoroutine);
+
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+
+    //Coroutines
+    IEnumerator DropColumns() {
+
+        while (true) {
+            yield return new WaitForSeconds(1);
+            Vector3 columnPosition = new Vector3(Random.Range(-4f, 4f), Random.Range(3f, 10f), Random.Range(-4f, 4f));
+            Quaternion columnRotation = new Quaternion(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90), 1);
+            //means rotation  x y z w(means 0-1)
+            GameObject newColumn = Instantiate(columnPrefab, columnPosition, columnRotation);
+        }
+    }
+}
+
